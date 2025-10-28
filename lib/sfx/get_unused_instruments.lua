092def918c1f8641cb5f7a87822652a4e4bcfe0d
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-27 20:49:55 -0400

    UPDATE - Use sfx_uses_instrument() in get_unused_instruments()

--- lib/sfx/get_unused_instruments.lua
+++ lib/sfx/get_unused_instruments.lua
@@ -1,39 +1,27 @@
 ---get unused custom instruments
 --
+--@depends
+--@see note_uses_instrument
+--@see sfx_uses_instrument
+--
 --@return {table} unused custom instrument indexes
 function get_unused_instruments()
 	local unused = {}
 
 	-- loop instrument indexes
-	for i = 0, 7 do
+	for inst_idx = 0, 7 do
 		-- loop sfx
 		for sfx_idx = 0, 63 do
-			local addr = 0x3200 + sfx_idx * 68
-
-			-- loop notes
-			for note_idx = 0, 31 do
-				local note = %(addr + note_idx * 2)
-
-				if
-					-- custom instrument bit
-					note & 0x8000 ~= 0
-					-- waveform is i
-					and note & 0x01c0 == i << 6
-					and (
-						-- volume is not 0
-						note & 0xe000 ~= 0
-						-- does not use slide cmd
-						or note & 0x7000 ~= 1 << 12
-					)
-				then
-					goto next_i
-				end
+			if
+				sfx_uses_instrument(sfx_idx, inst_idx)
+			then
+				goto next_instrument
 			end
 		end
 
-		add(unused, i)
+		add(unused, inst_idx)
 
-		::next_i::
+		::next_instrument::
 	end
 
 	return unused
