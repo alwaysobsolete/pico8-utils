af12a1193406bd94fbdd2a55185851444f137944
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-28 00:28:21 -0400

    UPDATE - Remove loop from sfx_volume()

--- lib/sfx/sfx_volume.lua
+++ lib/sfx/sfx_volume.lua
@@ -1,26 +1,16 @@
----modify volume of all sfx
+---modify sfx volume
 --
+-- @param sfx_idx {integer} - sfx index
 -- @param n {integer} volume to increment/decrement by
--- @param sfxstart {integer} start of sfx range to remove
--- @param sfxend {integer} end of sfx range to remove
-function sfx_volume(n, sfxstart, sfxend)
-	--loop through
-	--all non-instrument sfx
-	for sfx_num = sfxstart or 8, sfxend or 63 do
-		local sfx_addr = 0x3200 + sfx_num * 68
+function sfx_volume(sfx_idx, n)
+	local sfx_addr = 0x3200 + sfx_num * 68
 
-		--loop through all notes
-		for note_num = 0, 31 do
-			local note_addr = sfx_addr + note_num * 2
-			--get note from ram
-			local note = %note_addr
-			--get volume bits
-			local volume = (note & 0xe00) >>> 9
-			--calculate new volume
-			local newvolume = volume > 0 and mid(1, volume + n, 7) or 0
+	--loop through all notes
+	for note_addr = sfx_addr, sfx_addr + 63, 2 do
+		local note = %note_addr
+		local volume = (note & 0xe00) >>> 9
+		local newvolume = volume > 0 and mid(1, volume + n, 7) or 0
 
-			--insert new note
-			poke2(note_addr, note & 0xf1ff | (newvolume << 9))
-		end
+		poke2(note_addr, note & 0xf1ff | (newvolume << 9))
 	end
 end
